5b86aa6cea5929ff4823b8017d6d581354d7dd51
Teo <tiovine@novared.net>
2022-05-04 17:02:22 -0300

    adfgs

--- fetchIP.py
+++ fetchIP.py
@@ -1,25 +1,24 @@
 import urllib.request
+import uuid
 import json
 
-#test
+debug = 1
 
-guid = "5336ccd0-a833-40b7-ab39-ada7511db97f" #GUID para llamar al JSON de Microsoft (lo requiere)
-latestVersion = "0000000000" #Version inicial del JSON de Microsoft 
-							 #(es para saber si necesitas traerlo de nuevo o no)
-							 #Revisar, esto habria que traerlo de algun archivo.
-							 #Asi como esta, va a traer todo de nuevo todo el tiempo
-ws = "https://endpoints.office.com"
+# Funcion para hacer los llamados a O365
+# 	method: version: trae versiones; endpoints: trae urls/ips
+# 	scope: Worldwide: todos. No vamos a usar otro, pero conviene dejarlo asi
+def urlHelper(method,scope):
+	ws = "https://endpoints.office.com"
+	guid = str(uuid.uuid4())
+	requestPath = ws+'/'+method+'/'+scope+'?ClientRequestId='+guid
+	return urllib.request.urlopen(requestPath).read()
 
-#Primero chequeamos si necesitamos o no traer las IP
-#Si no hay dif entre latestVersion y la version aca, no traemos
+# Funcion para ver si necesitamos actualizar
+# latestVersion: ultima version registrada por el applet
+# Devuelve True si necesita actualizar, False de lo contrario
+def checkVersion():
+	return json.loads(urlHelper("version","Worldwide"))['latest']
 
-#armamos la peticion
-requestPath = ws+'/'+'version'+'/'+'Worldwide'+'?ClientRequestId='+guid
-o365Version = json.loads(urllib.request.urlopen(requestPath).read())['latest']
-#Comparamos latestVersion con la version que nos trajimos
-if latestVersion < o365Version:
-	#la version que tenemos es menos reciente que la que trajimos
-	latestVersion = o365Version
-	print(latestVersion)
-else:
-	#la version que tenemos es igual o mas reciente
+# Funcion para traernos las IP de O365
+def getIps():
+	return json.loads(urlHelper("endpoints","Worldwide"))
