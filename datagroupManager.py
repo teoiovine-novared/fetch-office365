3c0ae7e64c49ea0b6439a033192d56497ff3b8a6
Teo <tiovine@novared.net>
2022-05-06 17:23:26 -0300

    Viernes 06/05
    fetchIP.py:
    * Se agregó la función "getDataGroup()" para traer los datagroup del f5
    * Se pasó de "urllib" a "requests". Es más simple
    
    dataGroupManager.py:
    * Se emprolijó algunos restos de debuggig
    * Se agregaron parámetros de ejecución
    * Se comenzó el trabajo con el dataGroup

--- datagroupManager.py
+++ datagroupManager.py
@@ -1,28 +1,33 @@
 import fetchIP
-# TODO: Esto esta mal. latestVersion tiene que quedar guardado en un archivo
-# y bash tiene que ejecutar este script como main y enviar esa version como argumento.
-# En el script de iRuleLX se podía obviar porque quedaba guardado en memoria.
-# Pero no queremos hacer eso
+import sys
 
-o365Version = fetchIP.checkVersion()
+# Argumentos de llamada
+# TODO: Hacer que no importe el orden
 
-try:
-	if o365Version > latestVersion:
-		latestVersion = o365Version
-		print("1")
-	else:
-		print("2")
-except NameError:
-	latestVersion = o365Version
-	print("3")
-except:
-	print("4")
+f5Host = str(sys.argv[1])
+dataGroupName = str(sys.argv[2])
+latestVersion = int(sys.argv[3])
+user = str(sys.argv[4])
+passwd = str(sys.argv[5])
+verify = bool(sys.argv[6])
 
-#if "latestVersion" in locals() or 
-#	for x in o365List:
-# 		No todos los id tienen IP. Si lo mostras asi nomas, te da error.
-# 		Por eso chequeamos si existe primero
-#		if "ips" in x:
-#			print(x['ips'])
-#else:
-#	pass
+o365Version = int(fetchIP.checkVersion())
+
+if o365Version > latestVersion:
+	o365List = fetchIP.getIps()
+	dataGroup = fetchIP.getDataGroup(f5Host,dataGroupName,user,passwd,verify)
+	# TODO: Pensar como actualizar el datagroup.
+	# Hay que poner un chequeo para ver si existe en primera instancia
+	# Una vez que está creado, borrarlo y crearlo de nuevo si hay updates no me parece una opcion
+	# Si el volumen de traifoc es muy grande, ese interim, aunque breve, puede generar errores.
+	# Lo ideal sería chequear la diferencia. ¿Cómo? 
+	for x in dataGroup['records']:
+		print(x['name'])
+	for x in o365List:
+		# No todos los id tienen IP. Si lo mostras asi nomas, te da error.
+		# Por eso chequeamos si existe primero
+		if "ips" in x:
+			pass
+			#print(x['ips'])
+else:
+	print("Tenemos la ultima version")
--- fetchIP.py
+++ fetchIP.py
@@ -1,6 +1,8 @@
-import urllib.request
 import uuid
 import json
+import ssl
+import requests
+
 
 debug = 1
 
@@ -11,14 +13,20 @@ def urlHelper(method,scope):
 	ws = "https://endpoints.office.com"
 	guid = str(uuid.uuid4())
 	requestPath = ws+'/'+method+'/'+scope+'?ClientRequestId='+guid
-	return urllib.request.urlopen(requestPath).read()
+	return requests.get(requestPath).text
 
 # Funcion para ver si necesitamos actualizar
 # latestVersion: ultima version registrada por el applet
-# Devuelve True si necesita actualizar, False de lo contrario
 def checkVersion():
 	return json.loads(urlHelper("version","Worldwide"))['latest']
 
 # Funcion para traernos las IP de O365
 def getIps():
 	return json.loads(urlHelper("endpoints","Worldwide"))
+
+# Funcion para traer el data group entero del F5
+def getDataGroup(host,dataGroup,user,passwd,verify):
+	requestPath = 'https://'+host+'/mgmt/tm/ltm/data-group/internal/'+dataGroup
+	return json.loads(requests.get(requestPath,verify,auth=(user, passwd)).text)
+
+# TODO: Funcion para meter las IP en el datagroup
