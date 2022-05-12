66cc130064e066cea31c421c14439cd49b8e6874
Teo <tiovine@novared.net>
2022-05-12 17:08:48 -0300

    Version funcionando

--- datagroupManager.py
+++ datagroupManager.py
@@ -1,33 +1,40 @@
 import fetchIP
 import sys
 
+# Defaults para los argumentos de llamada
+# Los usamos ahora para testear tranquilos
+
+f5Host = "10.100.107.32" # IP por defecto del F5 si no dan uno por argumento (TODO: 127.0.0.1)
+dataGroupName = "O365_list" # Nombre por defecto del datagroup si no dan uno por argumento
+latestVersion = 0 # Ultima version registrada. La ponemos en 0 si el programa comienza por primera vez (pongo en 0 para forzar)
+user = "admin" # (Para test, esto debería venir siempre por argumento)
+passwd = "123momia" # (idem)
+verify = bool(0)
+
 # Argumentos de llamada
 # TODO: Hacer que no importe el orden
 
-f5Host = str(sys.argv[1])
-dataGroupName = str(sys.argv[2])
-latestVersion = int(sys.argv[3])
-user = str(sys.argv[4])
-passwd = str(sys.argv[5])
-verify = bool(sys.argv[6])
+#f5Host = str(sys.argv[1])
+#dataGroupName = str(sys.argv[2])
+#latestVersion = int(sys.argv[3])
+#user = str(sys.argv[4])
+#passwd = str(sys.argv[5])
+#verify = bool(sys.argv[6])
 
 o365Version = int(fetchIP.checkVersion())
 
 if o365Version > latestVersion:
-	o365List = fetchIP.getIps()
-	dataGroup = fetchIP.getDataGroup(f5Host,dataGroupName,user,passwd,verify)
-	# TODO: Pensar como actualizar el datagroup.
-	# Hay que poner un chequeo para ver si existe en primera instancia
-	# Una vez que está creado, borrarlo y crearlo de nuevo si hay updates no me parece una opcion
-	# Si el volumen de traifoc es muy grande, ese interim, aunque breve, puede generar errores.
-	# Lo ideal sería chequear la diferencia. ¿Cómo? 
-	for x in dataGroup['records']:
-		print(x['name'])
-	for x in o365List:
+	o365List_raw = fetchIP.getIps()
+	o365List_unique = []
+	for x in o365List_raw:
 		# No todos los id tienen IP. Si lo mostras asi nomas, te da error.
 		# Por eso chequeamos si existe primero
 		if "ips" in x:
-			pass
-			#print(x['ips'])
+			for y in x['ips']:
+				if y not in o365List_unique:
+					# Solo agrego a la lista los valores que no están ya en ella
+					o365List_unique.append(y)
+	# Enviamos la lista al F5
+	print(fetchIP.patchDataGroup(f5Host,dataGroupName,user,passwd,verify,o365List_unique))
 else:
 	print("Tenemos la ultima version")
--- fetchIP.py
+++ fetchIP.py
@@ -25,8 +25,18 @@ def getIps():
 	return json.loads(urlHelper("endpoints","Worldwide"))
 
 # Funcion para traer el data group entero del F5
+# Era necesaria en otra version del script, pero ya no se utiliza
+# La dejo por si en un futuro sirve
 def getDataGroup(host,dataGroup,user,passwd,verify):
 	requestPath = 'https://'+host+'/mgmt/tm/ltm/data-group/internal/'+dataGroup
-	return json.loads(requests.get(requestPath,verify,auth=(user, passwd)).text)
+	return json.loads(requests.get(requestPath,verify = verify,auth=(user, passwd)).text)
 
-# TODO: Funcion para meter las IP en el datagroup
+# Funcion para subir el datagroup al F5
+def patchDataGroup(host,dataGroup,user,passwd,verify,records):
+	requestPath = 'https://'+host+'/mgmt/tm/ltm/data-group/internal/'+dataGroup
+	head = {'Content-Type':'application/json'} # Encabezado necesario para la peticion
+	# Este doble paso lo tuve que hacer para que me armara correctamente el JSON.
+	# JSON necesita que sea con comillas dobles, y las listas de python laburan con comillas simples
+	records_json = {"records":records}
+	records_json = json.dumps(json_records)
+	return json.loads(requests.patch(requestPath,records_json,verify=verify,auth=(user, passwd),headers=head).text)
