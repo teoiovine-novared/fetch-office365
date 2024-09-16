db998a290642f48773667b268e24a2a66c831668
teoiovine-novared <103454898+teoiovine-novared@users.noreply.github.com>
2024-09-16 16:16:23 -0300

    Update parameters.sh

--- parameters.sh
+++ parameters.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Parametros para o365_list_update.sh
+# Setear permisos estrictos para este archivo
 # Parameters for o365_list_update.sh
+# Set strict permissions for this file
 
 IP="127.0.0.1"
 NAME="O365_list"
