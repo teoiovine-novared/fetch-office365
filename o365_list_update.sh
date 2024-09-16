f79f923c6f4b7f4588227c74a32b4cda97736bd0
Teo Iovine <teoiovine@gmail.com>
2024-09-16 14:58:31 -0300

    Script ahora llama directamente, hace falta agregar directorios al PATH, depende del usuario

--- o365_list_update.sh
+++ o365_list_update.sh
@@ -2,12 +2,17 @@
 # Script para el crontab. Simplemente se trae las variables del otro archivo y
 # se las pasa al script de Python
 # "latest_version" puede ser eliminada, seria como eliminar cache.
+# Hay que agregar el directorio donde se deje el script a PATH:
+# Temporal: export PATH="/shared/fetch-office365:$PATH"
+# Permanente: agregar 'export PATH="/shared/fetch-office365:$PATH"' al final de .bashrc
 # Script for crontab. Pulls variables from parameters.sh and calls python
 # with parameters.
 # "latest_version" file can be removed, works like clearing cache.
+# Working directory must be added to PATH:
+# Temporal: export PATH="/shared/fetch-office365:$PATH"
+# Permanent: add 'export PATH="/shared/fetch-office365:$PATH"' at the end of .bashrc
 
 
-cd /home/admin/fetch-office365 # TODO: variable
 source parameters.sh 
 # Should change this messages to your preference
 logger -s "============================="
