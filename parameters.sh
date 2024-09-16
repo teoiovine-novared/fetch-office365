836383c0999aa8d71ee5667bb1383e70234823d0
Teo Iovine <teoiovine@gmail.com>
2024-09-16 16:22:24 -0300

    a

--- parameters.sh
+++ parameters.sh
@@ -2,8 +2,8 @@
 # Parametros para o365_list_update.sh
 # Parameters for o365_list_update.sh
 
-IP="10.100.107.32"
-NAME="O365_list"
-USER="admin"
-PASS="123momia"
-SSL="0"
+IP="10.100.107.32" # F5 IP
+NAME="O365_list" # datagroup name
+USER="admin" # API user
+PASS="123momia" # passwd TODO: hide
+SSL="0" # 0 to skip SSL verification
