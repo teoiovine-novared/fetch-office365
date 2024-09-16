81eac1eb680c977d94e848d1ad81479ef781db94
Teo Iovine <teoiovine@gmail.com>
2024-09-16 15:06:10 -0300

    parameters

--- parameters.sh
+++ parameters.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Parametros para o365_list_update.sh
 # Parameters for o365_list_update.sh
-# TODO: Ponerlos uno abajo de otro. Hacerlo me agrega un \r que jode en el script
-IP="10.100.107.32" NAME="O365_list" USER="admin" PASS="123momia" SSL="0"
+
+IP="127.0.0.1"
+NAME="O365_list"
+USER="admin"
+PASS="password"
+SSL="0"
